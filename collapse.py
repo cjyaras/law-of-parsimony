763a9eb4333aee6427af55288143d7090d49bab2
cjyaras <cjyaras@gmail.com>
2023-05-13 14:51:23 -0400

    best fit line collapse

--- collapse.py
+++ collapse.py
@@ -13,3 +13,8 @@ def compute_feature_collapse(features, labels):
     sigma_B = (class_means_global_centered @ class_means_global_centered.T) / num_classes
     
     return jnp.trace(sigma_W) / jnp.trace(sigma_B)
+
+def best_fit_line(feature_collapse_per_layer):
+    layer_indices = jnp.arange(len(feature_collapse_per_layer), dtype=float)
+    p = jnp.polyfit(layer_indices, jnp.log(feature_collapse_per_layer), deg=1)
+    return jnp.exp(p[0] * layer_indices + p[1])
