ae036e6eabf3a02b48973eed36db52838a9bc1d8
cjyaras <cjyaras@gmail.com>
2024-03-21 23:09:20 +0000

    changes

--- network.py
+++ network.py
@@ -1,8 +1,8 @@
 import jax.numpy as jnp
 import jax.random as random
+import numpy as np
 from jax import grad
 from jax.nn import relu
-
 from utils import compose, svd
 
 
@@ -16,6 +16,7 @@ def _init_weight_orth(key, shape, init_scale):
         weight = weight[:m, :]
     return weight
 
+
 def _init_weight_norm(key, shape, init_scale):
     m, n = shape
     d = jnp.minimum(m, n)
@@ -25,6 +26,7 @@ def _init_weight_norm(key, shape, init_scale):
     correct_factor = jnp.sqrt(d) / jnp.sqrt(m * n)
 
     return correct_factor * weight
+
 
 def _init_weight_unif(key, shape, init_scale):
     m, n = shape
@@ -36,31 +38,33 @@ def _init_weight_unif(key, shape, init_scale):
 
     return correct_factor * weight
 
-def init_net(key, input_dim, output_dim, width, depth, init_scale, init_type='orth'):
 
-    if init_type == 'orth':
-        init_func = _init_weight_orth 
-    elif init_type == 'norm':
+def init_net(key, input_dim, output_dim, width, depth, init_scale, init_type="orth"):
+    if init_type == "orth":
+        init_func = _init_weight_orth
+    elif init_type == "norm":
         init_func = _init_weight_norm
-    elif init_type == 'unif':
+    elif init_type == "unif":
         init_func = _init_weight_unif
     else:
-        raise ValueError('Init type not recognized')
-    
+        raise ValueError("Init type not recognized")
+
     keys = random.split(key, num=depth)
 
     weights = [init_func(keys[0], (width, input_dim), init_scale)]
-    for i in range(depth-2):
-        weights.append(init_func(keys[i+1], (width, width), init_scale))
+    for i in range(depth - 2):
+        weights.append(init_func(keys[i + 1], (width, width), init_scale))
     weights.append(init_func(keys[-1], (output_dim, width), init_scale))
 
     return weights
+
 
 def compute_end_to_end(weights):
     product = weights[0]
     for w in weights[1:]:
         product = w @ product
     return product
+
 
 def compute_outputs(weights, input_data, nonlinear=False):
     intermediates = [input_data]
@@ -78,8 +82,8 @@ def compute_outputs(weights, input_data, nonlinear=False):
     output = weights[-1] @ output
     return output, intermediates
 
-def compute_factor(init_weights, network_fn, loss_fn, grad_rank):
 
+def compute_factor(init_weights, network_fn, loss_fn, grad_rank):
     e2e_loss_fn = compose(loss_fn, network_fn)
 
     width = init_weights[0].shape[0]
@@ -89,23 +93,46 @@ def compute_factor(init_weights, network_fn, loss_fn, grad_rank):
     Ugrad, _, Vgrad = svd(grad_W1_t0)
     Va = init_weights[0].T @ Ugrad[:, grad_rank:] / init_scale
     Vb = Vgrad[:, grad_rank:]
-    V0 = Va @ svd(jnp.concatenate([Va, -Vb], axis=1))[2][:Va.shape[1], width:]
+    V0 = Va @ svd(jnp.concatenate([Va, -Vb], axis=1))[2][: Va.shape[1], width:]
     V = svd(V0)[0][:, ::-1]
 
-    return V
+    return V[:, : 2 * grad_rank]
 
-def compress_network(init_weights, V, grad_rank):
 
+def compute_factor_v2(init_weights, network_fn, loss_fn, grad_rank):
+    e2e_loss_fn = compose(loss_fn, network_fn)
+
+    grad_W1_t0 = grad(e2e_loss_fn)(init_weights)[0]
+    v1 = np.linalg.svd(grad_W1_t0, full_matrices=False)[2].T[:, :grad_rank]
+    v2 = np.linalg.svd(grad_W1_t0.T @ init_weights[0], full_matrices=False)[2].T[
+        :, :grad_rank
+    ]
+    V1_1 = np.linalg.svd(np.concatenate([v1, v2], axis=1), full_matrices=False).U
+    return V1_1
+
+
+def compute_factor_v3(init_weights, network_fn, loss_fn, grad_rank):
+    e2e_loss_fn = compose(loss_fn, network_fn)
+
+    grad_W1_t0 = grad(e2e_loss_fn)(init_weights)[0]
+    A = grad_W1_t0
+    B = grad_W1_t0.T @ init_weights[0]
+    V1_1 = np.linalg.svd(np.concatenate([A, B], axis=0), full_matrices=True)[2].T[
+        :, : 2 * grad_rank
+    ]
+    return V1_1
+
+
+def compress_network(init_weights, V1_1, grad_rank):
     width = init_weights[0].shape[0]
     depth = len(init_weights)
     init_scale = jnp.linalg.norm(init_weights[0]) / jnp.sqrt(width)
 
-    V1_1 = V[:, :2*grad_rank]
-    UL_1 = compute_end_to_end(init_weights) @ V1_1 / (init_scale ** depth)
+    UL_1 = compute_end_to_end(init_weights) @ V1_1 / (init_scale**depth)
 
     compressed_init_weights = [V1_1.T]
     compressed_init_weights += [
-        init_scale * jnp.eye(2*grad_rank) for _ in range(depth)
+        init_scale * jnp.eye(2 * grad_rank) for _ in range(depth)
     ]
     compressed_init_weights += [UL_1]
 
